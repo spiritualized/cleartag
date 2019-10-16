edece3a10aa8f8c50c0c477ddb2c992504dbf76e
spiritualized <jeff.warren@gmail.com>
2019-10-15 21:43:29 -0400

    Create repository structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cleartag
 
+Audio metadata library providing a transparent interface for reading and writing MP3, FLAC, and other popular formats.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name = 'cleartag',
+    packages = ['cleartag'],
+    version = '1.0.0',
+    description = 'Audio metadata library providing a transparent interface for reading and writing MP3, FLAC, and other popular formats',
+    url = 'https://github.com/spiritualized/cleartag',
+    download_url = 'https://github.com/spiritualized/cleartag/archive/v1.0.0.tar.gz',
+    keywords = ['metadata', 'mp3', 'flac', 'python', 'library'],
+    install_requires = [
+                    'bitstring>=3.1.6',
+                    'mutagen>=1.42.0',
+                ],
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
