e50485d18f34318ed4b6a6e56ab7683a5ef674e1
spiritualized <jeff.warren@gmail.com>
2020-05-19 21:58:14 -0400

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 setup(
     name = 'cleartag',
     packages = ['cleartag', 'cleartag.enums'],
-    version = '1.0.1',
+    version = '1.1.0',
     description = 'Audio metadata library providing a transparent interface for reading and writing MP3, FLAC, and other popular formats',
     url = 'https://github.com/spiritualized/cleartag',
-    download_url = 'https://github.com/spiritualized/cleartag/archive/v1.0.1.tar.gz',
+    download_url = 'https://github.com/spiritualized/cleartag/archive/v1.1.0.tar.gz',
     keywords = ['metadata', 'mp3', 'flac', 'lame', 'python', 'library'],
     install_requires = [
                     'bitstring>=3.1.6',
